32fe3d50986db73007dd949ca9cf99105f5e3dac
dylsheridan766 <dylsheridan@gmail.com>
2025-10-12 17:38:27 -0400

    added array lists to main

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,7 +4,97 @@ import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    //lists for students and tutors emails and passwords
+
+    public static ArrayList<String> studentAccounts = new ArrayList<String>();
+    public static ArrayList<String> tutorAccounts = new ArrayList<String>();
+    //lists of all of the students and tutors
+    static ArrayList<student> students = new ArrayList<student>();
+    static ArrayList<tutor> tutors = new ArrayList<tutor>();
+
+
+    abstract class user{
+        String first;
+        String last;
+        String email;
+        String password;
+        String phone;
+        String program;
+        String highest;
+
+        user(String first, String last, String email, String password, String phone, String program, String highest){
+            this.first = first;
+            this.last = last;
+            this.email = email;
+            this.password = password;
+            this.phone = phone;
+            this.program = program;
+            this.highest = highest;
+        }
+
+        public String getFirst(){
+            return first;
+        }
+        public String getLast(){
+            return last;
+        }
+        public String getEmail(){
+            return email;
+        }
+
+        public String getPassword(){
+            return password;
+        }
+        public String getPhone(){
+            return phone;
+        }
+        public String getProgram(){
+            return program;
+        }
+        public String getHighest(){
+            return highest;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setFirst(String first) {
+            this.first = first;
+        }
+
+        public void setLast(String last) {
+            this.last = last;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+        public void setProgram( String program){
+            this.program=program;
+        }
+        public void setHighest(String highest){
+            this.highest=highest;
+        }
+    }
+
+    class student extends user{
+        public student(String first, String last, String email, String password, String phone, String program) {
+            super( first,  last,  email,  password,  phone,  program,  program);
+
+        }
+    }
+    class tutor extends user{
+        public tutor(String first, String last, String email, String password, String phone, String highest) {
+            super( first,  last,  email,  password,  phone, "", highest);
+        }
+    }
+
+
 
     // Declare Button object
     private Button btnLogin;
