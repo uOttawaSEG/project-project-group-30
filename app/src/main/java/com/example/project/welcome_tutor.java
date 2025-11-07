fdb0b7eb3dcddf9e0e7b5e64a1550ca09c78fc97
Ghosti-ing <tessamaya101@gmail.com>
2025-11-07 10:50:33 -0500

    added callender view

--- app/src/main/java/com/example/project/welcome_tutor.java
+++ app/src/main/java/com/example/project/welcome_tutor.java
@@ -3,6 +3,8 @@ package com.example.project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +23,15 @@ public class welcome_tutor extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_tutor);
         // Link the button variable to the actual button in the layout
         btnLogout = findViewById(R.id.btnLogout);
+        CalendarView calendar = findViewById(R.id.calendar);
+        TextView dateView = findViewById(R.id.date_view);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "-" + (month + 1) + "-" + year;
+                dateView.setText(date);
+            }
+        });
 // when the button is clicked it does thew following instructions
         btnLogout.setOnClickListener(v -> {
             // create an Intent to navigate from the tutor welcome screen back to MainActivity
