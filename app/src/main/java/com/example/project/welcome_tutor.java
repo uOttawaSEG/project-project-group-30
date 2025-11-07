cb76157e243f38515f17588e81cb2886253dfd13
dylsheridan766 <dylsheridan@gmail.com>
2025-11-07 13:22:01 -0500

    updated welcome_tutor

--- app/src/main/java/com/example/project/welcome_tutor.java
+++ app/src/main/java/com/example/project/welcome_tutor.java
@@ -1,13 +1,17 @@
 package com.example.project;
 
+import static java.lang.Boolean.valueOf;
 import static java.security.AccessController.getContext;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,9 +21,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class welcome_tutor extends AppCompatActivity {
     // button for logging out the tutor and returning to the main screen
     private Button btnLogout;
+    public String slectedDate = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +50,11 @@ public class welcome_tutor extends AppCompatActivity {
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 String date = dayOfMonth + "-" + (month + 1) + "-" + year;
                 dateView.setText(date);
-
+                slectedDate=year+month+dayOfMonth +"";
             }
         });
+        calendar.setMinDate(1762473600000L);
+
 
 // when the button is clicked it does thew following instructions
         btnLogout.setOnClickListener(v -> {
@@ -50,23 +63,59 @@ public class welcome_tutor extends AppCompatActivity {
             startActivity(intent);
         });
 
-        btnCreate.setOnClickListener(v ->{
+        btnCheck.setOnClickListener(v ->{
             Intent intent = new Intent(welcome_tutor.this, Calendar_Lists.class);
             startActivity(intent);
         });
 
-        btnCheck.setOnClickListener(v ->{
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Your available sessions");
-            builder.setMessage("PUT TIMES FROM DATABASE");
+        btnCreate.setOnClickListener(v ->{
 
-            builder.setPositiveButton("Got it", new DialogInterface.OnClickListener() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Create a session");
+            builder.setMessage("Add a 30 minute time slot in millitary time ex: 1205 for 12:05pm");
+
+            final EditText start = new EditText(this);
+            start.setInputType(InputType.TYPE_CLASS_NUMBER);
+            builder.setView(start);
+            final EditText stop = new EditText(this);
+            start.setInputType(InputType.TYPE_CLASS_NUMBER);
+            builder.setView(stop);
+
+            builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
+                    String startTime=start.getText().toString();
+                    String endTime = stop.getText().toString();
+
+                    if(startTime!=null||Integer.valueOf(startTime)> 2400 || Integer.valueOf(startTime)<0){
+                        Toast.makeText(welcome_tutor.this, "Enter a valid start time", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(endTime!=null||Integer.valueOf(endTime)> 2400 || Integer.valueOf(endTime)<0){
+                        Toast.makeText(welcome_tutor.this, "Enter a valid end time", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(Integer.valueOf(startTime)>Integer.valueOf(endTime)){
+                        Toast.makeText(welcome_tutor.this, "The session must end after it starts", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else{
+                        int diff = 0;
+                        diff+=Integer.valueOf(endTime.substring(0,2))*60+Integer.valueOf(endTime.substring(3,5));
+                        diff-=Integer.valueOf(startTime.substring(0,2))*60+Integer.valueOf(startTime.substring(3,5));
+                        if(diff==30){
+
+                        }
+                        else{
+                            Toast.makeText(welcome_tutor.this, "The session must last 30 minutes", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+
                     dialog.dismiss(); // Close the dialog
                 }
             });
             AlertDialog alertDialog = builder.create();
             alertDialog.show();
         });
+
     }
 }
